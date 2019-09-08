6b9971d18a2981d64d550a3ea9baed35669db63e
Brad HAzel <bradhazel@live.ca>
2019-09-08 07:35:38 -0600

    added code to handle the refresh every min added code to store previous values in local storage add test on error message returned from api to decide where to pull the values from

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -8,16 +8,41 @@ import { AlphaVantageService } from '../alpha-vantage.service';
 })
 export class CardComponent implements OnInit {
 
+
   @Input()
   public currency: string;
 
   public rate: string;
-
-  constructor(private alphaVantageService: AlphaVantageService) { }
+  public rateNew: string;
+  public rateCurrency: string;
+  public rateStored: string[];
+	
+  constructor(private alphaVantageService: AlphaVantageService) {
+  }
 
   ngOnInit() {
-    this.alphaVantageService.get(this.currency).subscribe(result => {
-      this.rate = result['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+      this.alphaVantageService.get(this.currency).subscribe(result => {
+     //alert(result['Error Message']);
+
+      this.rateNew = result['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+      //if(result['Error Message'] === null)
+      {
+        this.rate = this.rateNew;
+        this.rateCurrency = result['Realtime Currency Exchange Rate']['4. To_Currency Name'];
+        this.rateNew = '';
+        //3. To_Currency Code
+  
+        localStorage.setItem(result['Realtime Currency Exchange Rate']['3. To_Currency Code'], JSON.stringify(result['Realtime Currency Exchange Rate']));
+      }
+      //else
+      //{ 
+        //alert(result['Error Message']);
+       // this.rateStored = JSON.parse(localStorage.getItem(this.currency));
+        //alert( this.rateStored['5. Exchange Rate']);
+       // this.rate = this.rateStored['5. Exchange Rate'];
+        //this.rateCurrency = "temp";
+        //this.rateNew = '';
+      //
     });
   }
 
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter, ChangeDetectorRef } from '@angular/core';
 import { AlphaVantageService } from '../alpha-vantage.service';
+import { Subscription, Observable, timer } from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,13 +9,47 @@ import { AlphaVantageService } from '../alpha-vantage.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  private subscription: Subscription;
 
+	@Input() SearchDate: moment.Moment = moment();
+	@Input() ElapsTime: number = 1;
   public currencies = ['CAD', 'JPY', 'EUR'];
+  searchEndDate: moment.Moment;
+	remainingTime: number;
+	minutes: number;
+	seconds: number;
 
-  constructor() { }
+	everySecond: Observable<number> = timer(0, 1000);
+
+    constructor( private ref: ChangeDetectorRef) {
+    this.searchEndDate = this.SearchDate.add(this.ElapsTime, "minutes");
+  }
 
   ngOnInit() {
+ 
+    this.subscription = this.everySecond.subscribe((seconds) => {
+			var currentTime: moment.Moment = moment();
+			this.remainingTime = this.searchEndDate.diff(currentTime)
+			this.remainingTime = this.remainingTime / 1000;
 
+			if (this.remainingTime <= 0) {
+				this.SearchDate = moment();
+				this.searchEndDate = this.SearchDate.add(this.ElapsTime, "minutes");
+
+        this.refresh()
+			}
+			else {
+				this.minutes = Math.floor(this.remainingTime / 60);
+				this.seconds = Math.floor(this.remainingTime - this.minutes * 60);
+			}
+			this.ref.markForCheck()
+		})
   }
+  ngOnDestroy(): void {
+		this.subscription.unsubscribe();
+	}
+  refresh(): void {
+    window.location.reload();
+}
 
 }
