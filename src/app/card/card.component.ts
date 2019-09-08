99a817de46bc8d1bd1a0507512eb19955b4cc038
Brad HAzel <bradhazel@live.ca>
2019-09-08 08:54:30 -0600

    code clean up added last clean exchange date to display

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -13,8 +13,11 @@ export class CardComponent implements OnInit {
   public currency: string;
 
   public rate: string;
+  public rateArray: string[];
+  public errorMsg: string;
   public rateNew: string;
   public rateCurrency: string;
+  public rateRefreshDate: string;
   public rateStored: string[];
 	
   constructor(private alphaVantageService: AlphaVantageService) {
@@ -22,27 +25,28 @@ export class CardComponent implements OnInit {
 
   ngOnInit() {
       this.alphaVantageService.get(this.currency).subscribe(result => {
-     //alert(result['Error Message']);
+
+      this.errorMsg = result['Error Message']
 
       this.rateNew = result['Realtime Currency Exchange Rate']['5. Exchange Rate'];
-      //if(result['Error Message'] === null)
+            
+      if(typeof result['Error Message'] === "undefined" && this.rateNew != "")
       {
-        this.rate = this.rateNew;
-        this.rateCurrency = result['Realtime Currency Exchange Rate']['4. To_Currency Name'];
-        this.rateNew = '';
-        //3. To_Currency Code
-  
         localStorage.setItem(result['Realtime Currency Exchange Rate']['3. To_Currency Code'], JSON.stringify(result['Realtime Currency Exchange Rate']));
-      }
-      //else
-      //{ 
-        //alert(result['Error Message']);
-       // this.rateStored = JSON.parse(localStorage.getItem(this.currency));
-        //alert( this.rateStored['5. Exchange Rate']);
-       // this.rate = this.rateStored['5. Exchange Rate'];
-        //this.rateCurrency = "temp";
+        //this.rate = this.rateNew;
+        //this.rateCurrency = result['Realtime Currency Exchange Rate']['4. To_Currency Name'];
+        //this.rateRefreshDate  = result['Realtime Currency Exchange Rate']['6. Last Refreshed'];
         //this.rateNew = '';
-      //
+      }
+  
+        //alert(result['Error Message']);
+        this.rateStored = JSON.parse(localStorage.getItem(this.currency));
+        //alert( this.rateStored['5. Exchange Rate']);
+        this.rate = this.rateStored['5. Exchange Rate'];
+        this.rateCurrency = this.rateStored['4. To_Currency Name'];
+        this.rateRefreshDate  = this.rateStored['6. Last Refreshed'] + " " +this.rateStored['7. Time Zone']; //'7. Time Zone' ;
+        his.rateNew = '';
+      
     });
   }
 
